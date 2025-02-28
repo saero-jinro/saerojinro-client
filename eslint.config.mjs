661a407b653c46a1dda7da6143c101ad723bb78e
hjoo830 <hjoo830@naver.com>
2025-02-28 14:10:47 +0900

    chore: update ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import prettierPlugin from "eslint-plugin-prettier";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,34 +11,39 @@ const compat = new FlatCompat({
 });
 
 const ignoreConfig = {
-  ignores: ["**/.next/**", "**/node_modules/**", "**/public/**", "**/dist/**"],
+  ignores: ['**/.next/**', '**/node_modules/**', '**/public/**', '**/dist/**'],
 };
 
 const eslintConfig = [
   ignoreConfig,
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
     plugins: {
       prettier: prettierPlugin,
     },
 
-    files: ["src/**/*.{ts,tsx}"],
+    files: ['src/**/*.{ts,tsx}'],
 
     rules: {
-      semi: ["error", "always"],
-      "react/function-component-definition": [
-        "error",
+      semi: ['error', 'always'],
+      'react/function-component-definition': [
+        'error',
         {
-          namedComponents: "arrow-function",
-          unnamedComponents: "arrow-function",
+          namedComponents: 'arrow-function',
+          unnamedComponents: 'arrow-function',
         },
       ],
-      indent: ["error", 2, { SwitchCase: 1 }],
-      "prettier/prettier": ["error", 
-        { 
+      indent: ['error', 2, { SwitchCase: 1 }],
+      'prettier/prettier': [
+        'error',
+        {
           printWidth: 100,
-          "endOfLine":"auto",
-        }],
+          endOfLine: 'auto',
+        },
+      ],
+      quotes: ['error', 'single', { avoidEscape: true }],
+      'array-bracket-newline': ['error', { multiline: true }],
+      'array-element-newline': ['error', 'always'],
     },
   },
 ];
