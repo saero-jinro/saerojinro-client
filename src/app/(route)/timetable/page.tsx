113a9eb5eead112e3e5b86578aba49bc12970a85
hjoo830 <hjoo830@naver.com>
2025-03-06 19:35:37 +0900

    feat: add right panel for empty slot selection

--- src/app/(route)/timetable/page.tsx
+++ src/app/(route)/timetable/page.tsx
@@ -23,6 +23,7 @@ interface UserLectures {
 
 const TimetablePage = () => {
   const [userLectures, setUserLectures] = useState<UserLectures>({ reservation: [], wishlist: [] });
+  const [selectedTime, setSelectedTime] = useState<string | null>(null);
   const router = useRouter();
   const currentUserId = 222; // 현재 로그인한 유저 ID 가져오는 로직 추가
 
@@ -81,92 +82,175 @@ const TimetablePage = () => {
   const occupiedSlots = new Set();
 
   return (
-    <div className="p-10">
-      <h1 className="text-xl font-bold mb-4">컴퍼런스 일정 시간표</h1>
+    <div className="flex gap-4">
+      <div className="flex-2 p-10">
+        <h1
+          className={`text-xl font-bold mb-4 transition-all ${
+            selectedTime ? 'text-left' : 'text-center w-full'
+          }`}
+        >
+          컴퍼런스 일정 시간표
+        </h1>
 
-      <div
-        className="grid border border-gray-300"
-        style={{ gridTemplateColumns: '140px repeat(3, 1fr)' }}
-      >
-        <div className="border border-gray-300 p-3 font-bold text-center">Time</div>
-        <div className="border border-gray-300 p-3 font-bold text-center">Day1</div>
-        <div className="border border-gray-300 p-3 font-bold text-center">Day2</div>
-        <div className="border border-gray-300 p-3 font-bold text-center">Day3</div>
+        <div
+          className="grid border border-gray-300"
+          style={{ gridTemplateColumns: '140px repeat(3, 1fr)' }}
+        >
+          <div className="border border-gray-300 p-3 font-bold text-center">Time</div>
+          <div className="border border-gray-300 p-3 font-bold text-center">Day1</div>
+          <div className="border border-gray-300 p-3 font-bold text-center">Day2</div>
+          <div className="border border-gray-300 p-3 font-bold text-center">Day3</div>
 
-        {timeSlots.map(({ start, end }, i) => (
-          <Fragment key={`time-${start}-${end}`}>
-            <div className="flex justify-center items-center border border-gray-300 bg-gray-100 dark:bg-gray-600 p-3 text-center font-medium h-16">
-              {start} ~ {end}
-            </div>
+          {timeSlots.map(({ start, end }, i) => (
+            <Fragment key={`time-${start}-${end}`}>
+              <div className="flex justify-center items-center border border-gray-300 bg-gray-100 dark:bg-gray-600 p-3 text-center font-medium">
+                {start} ~ {end}
+              </div>
 
-            {Array.from({ length: 3 }, (_, j) => {
-              const day = j + 1;
+              {Array.from({ length: 3 }, (_, j) => {
+                const day = j + 1;
 
-              // 이미 배치된 강의는 렌더링 x
-              if (occupiedSlots.has(`${day}-${start}`)) {
-                return null;
-              }
+                // 이미 배치된 강의는 렌더링 x
+                if (occupiedSlots.has(`${day}-${start}`)) {
+                  return null;
+                }
 
-              const lecture = userLectures.reservation.find((lec) => {
-                const formattedStartTime = new Date(lec.start_time).toLocaleTimeString('ko-KR', {
-                  hour: '2-digit',
-                  minute: '2-digit',
-                  hour12: false,
-                });
-                const formattedEndTime = new Date(lec.end_time).toLocaleTimeString('ko-KR', {
-                  hour: '2-digit',
-                  minute: '2-digit',
-                  hour12: false,
-                });
-
-                return lec.day === day && formattedStartTime <= start && formattedEndTime > start;
-              });
-
-              if (lecture) {
-                const rowSpan =
-                  (new Date(lecture.end_time).getTime() - new Date(lecture.start_time).getTime()) /
-                  (60 * 60 * 1000);
-
-                for (let k = 0; k < rowSpan; k++) {
-                  const occupiedTime = new Date(
-                    new Date(lecture.start_time).getTime() + k * 60 * 60 * 1000,
-                  ).toLocaleTimeString('ko-KR', {
+                const lecture = userLectures.reservation.find((lec) => {
+                  const formattedStartTime = new Date(lec.start_time).toLocaleTimeString('ko-KR', {
                     hour: '2-digit',
                     minute: '2-digit',
                     hour12: false,
                   });
-                  occupiedSlots.add(`${day}-${occupiedTime}`);
-                }
+                  const formattedEndTime = new Date(lec.end_time).toLocaleTimeString('ko-KR', {
+                    hour: '2-digit',
+                    minute: '2-digit',
+                    hour12: false,
+                  });
 
-                return (
-                  <div
-                    key={`day-${day}-time-${start}`}
-                    className="flex flex-col justify-center gap-2 border border-gray-300 p-4 cursor-pointer"
-                    style={{ gridRow: `span ${rowSpan}` }}
-                    onClick={() => router.push(`/lecture/${lecture?.id}`)} // 강의 상세 모달로 변경하기
-                  >
-                    <p className="bg-gray-200 w-fit px-2 dark:bg-gray-600">{lecture.location}</p>
-                    <span>{lecture?.title}</span>
-                    <div className="flex justify-between w-full">
-                      <p>{lecture.speaker}</p>
-                      <p>
-                        인원수 {lecture.count} / {lecture.total}
-                      </p>
+                  return lec.day === day && formattedStartTime <= start && formattedEndTime > start;
+                });
+
+                if (lecture) {
+                  const rowSpan =
+                    (new Date(lecture.end_time).getTime() -
+                      new Date(lecture.start_time).getTime()) /
+                    (60 * 60 * 1000);
+
+                  for (let k = 0; k < rowSpan; k++) {
+                    const occupiedTime = new Date(
+                      new Date(lecture.start_time).getTime() + k * 60 * 60 * 1000,
+                    ).toLocaleTimeString('ko-KR', {
+                      hour: '2-digit',
+                      minute: '2-digit',
+                      hour12: false,
+                    });
+                    occupiedSlots.add(`${day}-${occupiedTime}`);
+                  }
+
+                  return (
+                    <div
+                      key={`day-${day}-time-${start}`}
+                      className="flex flex-col justify-center gap-2 border border-gray-300 p-4 cursor-pointer"
+                      style={{ gridRow: `span ${rowSpan}` }}
+                      onClick={() => router.push(`/lecture/${lecture?.id}`)} // 강의 상세 모달로 변경하기
+                    >
+                      <p className="bg-gray-200 w-fit px-2 dark:bg-gray-600">{lecture.location}</p>
+                      <span>{lecture?.title}</span>
+                      <div className="flex justify-between w-full">
+                        <p>{lecture.speaker}</p>
+                        <p>
+                          인원수 {lecture.count} / {lecture.total}
+                        </p>
+                      </div>
                     </div>
-                  </div>
-                );
-              } else {
-                return (
-                  <div
-                    key={`day-${day}-time-${start}`}
-                    className="border border-gray-300 p-3 bg-gray-100 dark:bg-gray-600 hover:bg-gray-300"
-                  />
-                );
-              }
-            })}
-          </Fragment>
-        ))}
+                  );
+                } else {
+                  return (
+                    <div
+                      key={`day-${day}-time-${start}`}
+                      className="border border-gray-300 p-3 bg-gray-100 dark:bg-gray-600 hover:bg-gray-300"
+                      onClick={() => {
+                        setSelectedTime(`Day${day} ${start}`);
+                        console.log(selectedTime);
+                      }}
+                    />
+                  );
+                }
+              })}
+            </Fragment>
+          ))}
+        </div>
       </div>
+
+      {selectedTime && (
+        <div className="flex-1 p-4 pt-10 bg-gray-50 h-screen dark:bg-gray-900">
+          <h3 className=" text-md font-semibold">즐찾 목록</h3>
+          <div className="h-1/3 overflow-auto">
+            <ul className="mt-2 border">
+              {userLectures.wishlist.map((lecture) => (
+                <li key={lecture.id} className="p-2 border-b border-gray-200">
+                  <div className="flex items-center gap-2">
+                    <p className="bg-gray-200 w-fit px-2 dark:bg-gray-600">{lecture.location}</p>
+                    <span className="text-xs">
+                      {new Date(lecture?.start_time).toLocaleTimeString('ko-KR', {
+                        hour: '2-digit',
+                        minute: '2-digit',
+                        hour12: false,
+                      })}{' '}
+                      -{' '}
+                      {new Date(lecture?.end_time).toLocaleTimeString('ko-KR', {
+                        hour: '2-digit',
+                        minute: '2-digit',
+                        hour12: false,
+                      })}
+                    </span>
+                  </div>
+                  <span>{lecture?.title}</span>
+                  <div className="flex justify-between w-full">
+                    <p>{lecture.speaker}</p>
+                    <button className="bg-gray-600 text-white p-1 text-xs dark:bg-gray-500 cursor-pointer">
+                      강의 신청
+                    </button>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <h3 className="pt-10 text-md font-semibold">공석 추천 목록</h3>
+          <div className="h-1/2 overflow-auto">
+            <ul className="mt-2 border">
+              {/* 추천 강의로 바꾸기 */}
+              {userLectures.wishlist.map((lecture) => (
+                <li key={lecture.id} className="p-2 border-b border-gray-200">
+                  <div className="flex items-center gap-2">
+                    <p className="bg-gray-200 w-fit px-2 dark:bg-gray-600">{lecture.location}</p>
+                    <span className="text-xs">
+                      {new Date(lecture?.start_time).toLocaleTimeString('ko-KR', {
+                        hour: '2-digit',
+                        minute: '2-digit',
+                        hour12: false,
+                      })}{' '}
+                      -{' '}
+                      {new Date(lecture?.end_time).toLocaleTimeString('ko-KR', {
+                        hour: '2-digit',
+                        minute: '2-digit',
+                        hour12: false,
+                      })}
+                    </span>
+                  </div>
+                  <span>{lecture?.title}</span>
+                  <div className="flex justify-between w-full">
+                    <p>{lecture.speaker}</p>
+                    <button className="bg-gray-600 text-white p-1 text-xs dark:bg-gray-500 cursor-pointer">
+                      강의 신청
+                    </button>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
