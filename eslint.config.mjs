346808eb72a76a20784beb1724b9129cd4849902
ki2183 <ki2183@naver.com>
2025-03-13 22:12:19 +0900

    chore(eslint): 버튼 라벨링을 제외한 규칙 제외
    Resolves #45

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,7 +17,7 @@ const ignoreConfig = {
 
 const eslintConfig = [
   ignoreConfig,
-  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier','plugin:jsx-a11y/recommended'),
   {
     plugins: {
       prettier: prettierPlugin,
@@ -42,6 +42,19 @@ const eslintConfig = [
       'array-bracket-newline': 'off',
 
       'prettier/prettier': ['error', prettierConfig],
+
+      'jsx-a11y/click-events-have-key-events': 'warn',  // 클릭 이벤트가 있는 요소에 키보드 이벤트 요구 
+      'jsx-a11y/no-static-element-interactions': 'off',  // span, div 요소에서 이벤트 사용시 오류
+      'jsx-a11y/no-noninteractive-element-interactions': 'off',  // 비대화 요소에서 이벤트 사용시 오류
+      'jsx-a11y/interactive-supports-focus': 'off', // 포커스 지원 요구
+
+      'jsx-a11y/control-has-associated-label': [
+        'warn', 
+        {
+          controlComponents: ['button', 'input'], 
+          ignoreElements: ['textarea'], 
+        },
+      ],
     },
   },
 ];
