1246b968033eeaf55bd243717b8741f96451153f
ki2183 <ki2183@naver.com>
2025-03-23 10:37:44 +0900

    feat(mainPage): 네비게이션 기능 버튼 추가 및 스크롤에 따른 스타일 변경
    Resolve: #92

--- src/app/(route)/(main)/mainComponent/component/SectionList.tsx
+++ src/app/(route)/(main)/mainComponent/component/SectionList.tsx
@@ -1,13 +1,15 @@
 'use client';
 
 import { useLectureStore } from '@/_store/LectureList/useLectureStore';
+import SkeletonCard from '@/_components/Card/SkeletonCard';
+import { formatTime } from '@/_utils/Card/formatTime';
 import { useEffect, useRef, useState } from 'react';
-import OptionList from './OptionList';
 import ScrollWrapper from './ScrollWrapper';
 import Card from '@/_components/Card/Card';
-import { formatTime } from '@/_utils/Card/formatTime';
-import { Lectures } from '@/_types/Lectures/lectures.type';
-import { getLectures } from '@/_utils/Main/getLectures';
+import OptionList from './OptionList';
+import { Lectures, ResponseLectures } from '../type/lectures.type';
+import { wrapApiResponse } from '@/_utils/api/response';
+import { ApiResponse } from '@/_types/Auth/auth.type';
 
 export interface LectureList {
   id: number;
@@ -22,49 +24,79 @@ export interface LectureList {
 interface Props {
   startDay: number;
   startDate: string;
-  initLectures: Lectures;
-  isLecturesFetched: boolean;
+  initLectures?: Lectures;
 }
 
-const SectionList = ({ startDay, startDate, initLectures, isLecturesFetched }: Props) => {
+// 강의 데이터 추출 훅
+const getLectures = async (date: string, day: number): Promise<ApiResponse<ResponseLectures>> => {
+  const BACK_URL = process.env.NEXT_PUBLIC_BACKEND_API;
+  const formattedDay = day.toString().padStart(2, '0');
+  const fullDate = `${date}-${formattedDay}`;
+  const URL = `${BACK_URL}/api/lectures/date?date=${fullDate}`;
+
+  return wrapApiResponse(
+    () => fetch(`${URL}`),
+    (res) => res.json().then((dto: ResponseLectures) => dto),
+  );
+};
+
+// 컴포넌트
+const SectionList = ({ startDay, startDate }: Props) => {
   const [lectureOption, setLectureOption] = useState<number>(startDay); // 강의 옵션 상태
-  const [lectures, setLectures] = useState<Lectures>(initLectures);
-  const isInitialLoad = useRef<boolean>(isLecturesFetched); // 초기값 패치 여부
+  const [lectures, setLectures] = useState<Lectures>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [prevNum, setPrevNum] = useState<number>(5);
   const { wishlist } = useLectureStore();
 
-  // lectureOption에 따른 API 요청
-  useEffect(() => {
-    if (isInitialLoad.current) {
-      isInitialLoad.current = false;
-      return; // 첫 요청은 서버에서 처리했으므로 무시함
-    }
+  // 캐싱
+  const cacheRef = useRef<Map<string, Lectures>>(new Map());
 
+  // 세션 데이터 가져오기
+  useEffect(() => {
+    const cacheKey = `${startDate}-${lectureOption}`;
     (async () => {
+      setLoading(true);
+
+      // 데이터 캐싱 처리
+      if (cacheRef.current.has(cacheKey)) {
+        const cachedData = cacheRef.current.get(cacheKey)!;
+        setLectures(cachedData);
+        setPrevNum(cachedData.length);
+        return;
+      }
+
       try {
-        const data = await getLectures(startDate, lectureOption);
-        console.log(data);
-        setLectures(data);
+        const res = await getLectures(startDate, lectureOption);
+
+        if (!res.data) throw new Error('데이터 없음');
+
+        const data = res.data as ResponseLectures;
+        cacheRef.current.set(cacheKey, data.lectures);
+        setLectures(data.lectures);
+        setPrevNum(data.totalCount);
+        setLoading(false);
       } catch (error) {
         console.error('강의 로드 실패: 테스트 데이터로 대체', error);
       }
     })();
   }, [lectureOption, startDate]);
 
+  // 옵션 변경시 로딩 초기화
   useEffect(() => {
-    console.log(lectures);
-  }, [lectures]);
+    setLoading(false);
+  }, [lectureOption]);
 
   return (
-    <>
+    <div>
       <OptionList
-        className="mt-6"
         startDay={startDay}
         option={lectureOption}
         ChangeOption={(option: number) => setLectureOption(option)}
       />
       {/* 강의 카드 리스트 */}
-      <ScrollWrapper>
-        {lectures.length === 0 && <div className="h-[222px]">강의 리스트를 불러오는 중...</div>}
+      <ScrollWrapper className="mt-3">
+        {loading && Array.from({ length: prevNum }).map((_, i) => <SkeletonCard key={i} />)}
+
         {lectures.map((lecture, idx) => (
           <Card
             key={idx}
@@ -79,7 +111,7 @@ const SectionList = ({ startDay, startDate, initLectures, isLecturesFetched }: P
           />
         ))}
       </ScrollWrapper>
-    </>
+    </div>
   );
 };
 
