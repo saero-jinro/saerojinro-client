91df49c4a913c61a619e8be7b60f586154735c3b
ki2183 <ki2183@naver.com>
2025-03-23 10:31:54 +0900

    design(mainPage): footer 마진 패딩 조정
    Resolve: #92

--- src/app/(route)/(main)/mainComponent/_MainFooter.tsx
+++ src/app/(route)/(main)/mainComponent/_MainFooter.tsx
@@ -1,13 +1,12 @@
 const Footer = () => {
   return (
-    <footer
-      aria-label="footer"
-      className="px-[40px] py-[48px] min-w-[768px] max-w-[1280] flex flex-col self-stretch text-white bg-[#202427] gap-6"
-    >
-      <h2 className="font-bold text-4xl">IT TIME</h2>
-      <div className="flex flex-col gap-6">
-        <span>© 2025 IT Conference. All rights reserved</span>
-        <span>개인정보방침</span>
+    <footer aria-label="footer" className="px-[40px] py-[48px] text-white bg-[#202427] flex">
+      <div className="flex flex-col self-stretch max-w-[1280px] gap-6 mx-auto w-full ">
+        <h2 className="font-bold text-4xl">IT TIME</h2>
+        <div className="flex flex-col gap-6">
+          <span>© 2025 IT Conference. All rights reserved</span>
+          <span>개인정보방침</span>
+        </div>
       </div>
     </footer>
   );
