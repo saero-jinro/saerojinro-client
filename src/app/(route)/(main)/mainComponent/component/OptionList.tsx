46342e1316693e52ed7f5c45bf0bf8c82445fd25
ki2183 <ki2183@naver.com>
2025-03-25 14:35:56 +0900

    feat(mainPage): enum 기반 강의 데이터 필터링 기능 추가
    - 초기 강의 데이터를 가져와 state에 저장
    - 저장된 데이터를 기반으로 필터링 수행
    - 필터링 결과는 state에 캐싱하여 재사용
    
    Resolves: #95

--- src/app/(route)/(main)/mainComponent/component/OptionList.tsx
+++ src/app/(route)/(main)/mainComponent/component/OptionList.tsx
@@ -1,28 +1,27 @@
 import ClickButton from '@/_components/ClickButton';
 import { HTMLAttributes, ReactNode } from 'react';
+import { CategoryLabel, CategoryWithAll } from './SectionList';
 
 interface Props {
-  option: number;
-  startDay: number;
-  ChangeOption: (option: number) => void;
+  option: CategoryWithAll;
+  ChangeOption: (option: CategoryWithAll) => void;
   className?: string;
   limit?: number;
 }
 
-const OptionList = ({ option, limit = 3, ChangeOption, className, startDay }: Props) => {
-  const options = [...Array(limit).keys()].map((idx) => startDay + idx);
-  const getScript = (opt: number) => `${opt}일`;
+export const OptionList = ({ option, ChangeOption, className }: Props) => {
+  const options = Object.entries(CategoryLabel) as [CategoryWithAll, string][];
 
   return (
-    <ul className={`flex gap-[0.5rem] select-none ${className}`}>
-      {options.map((opt) => (
+    <ul className={`flex gap-[0.5rem] select-none flex-wrap ${className}`}>
+      {options.map(([category, title]) => (
         <OptionItem
-          key={opt}
-          desc={`item-change-${opt}`}
-          onClickAction={() => ChangeOption(opt)}
-          className={`${option === opt ? 'bg-[#000000] text-white' : 'bg-[#9A9A9A] text-[#49454f]'}`}
+          key={category}
+          desc={`item-change-${category}`}
+          onClickAction={() => ChangeOption(category)}
+          className={`${option === category ? 'bg-[#000000] text-white' : 'bg-[#9A9A9A] text-[#49454f]'}`}
         >
-          {getScript(opt)}
+          {title}
         </OptionItem>
       ))}
     </ul>
@@ -39,9 +38,13 @@ const OptionItem = ({ desc, onClickAction, children, className, ...props }: Opti
   return (
     <li
       {...props}
-      className={`select-none outline-none text-sm h-8 py-[6px] px-4 rounded-[8px] flex items-center justify-center whitespace-nowrap ${className}`}
+      className={`select-none outline-none text-sm h-8 rounded-[8px] flex items-center justify-center whitespace-nowrap ${className}`}
     >
-      <ClickButton actionDesc={desc} onClickAction={onClickAction}>
+      <ClickButton
+        className="w-full h-full py-[6px] px-4"
+        actionDesc={desc}
+        onClickAction={onClickAction}
+      >
         {children}
       </ClickButton>
     </li>
--- src/app/(route)/(main)/mainComponent/component/SectionList.tsx
+++ src/app/(route)/(main)/mainComponent/component/SectionList.tsx
@@ -3,13 +3,14 @@
 import { useLectureStore } from '@/_store/LectureList/useLectureStore';
 import SkeletonCard from '@/_components/Card/SkeletonCard';
 import { formatTime } from '@/_utils/Card/formatTime';
-import { useEffect, useRef, useState } from 'react';
+import { startTransition, useCallback, useEffect, useRef, useState } from 'react';
 import ScrollWrapper from './ScrollWrapper';
 import Card from '@/_components/Card/Card';
 import OptionList from './OptionList';
 import { Lectures, ResponseLectures } from '../type/lectures.type';
 import { wrapApiResponse } from '@/_utils/api/response';
 import { ApiResponse } from '@/_types/Auth/auth.type';
+import { Category } from '@/_types/Write/write.type';
 
 export interface LectureList {
   id: number;
@@ -27,13 +28,31 @@ interface Props {
   initLectures?: Lectures;
 }
 
+export type CategoryWithAll = Category | 'ALL';
+
+export const CategoryLabel: Record<CategoryWithAll, string> = {
+  ALL: '전체',
+  BACKEND: '백엔드',
+  FRONTEND: '프론트엔드',
+  AI: '인공지능 / 머신러닝',
+  DATA: '데이터 분석',
+  CLOUD: '클라우드 인프라',
+  DEVOPS: 'DevOps / 자동화',
+  UX_UI: 'UX·UI 디자인',
+  SEC: '보안 / 해킹 대응',
+  PM: '기획·PM',
+  BLOCKCHAIN: '블록체인',
+  MOBILE: '모바일 앱',
+};
+
+type CategoryState = Record<CategoryWithAll, ResponseLectures & { load: boolean }>;
+
 // 강의 데이터 추출 훅
 const getLectures = async (date: string, day: number): Promise<ApiResponse<ResponseLectures>> => {
   const BACK_URL = process.env.NEXT_PUBLIC_BACKEND_API;
   const formattedDay = day.toString().padStart(2, '0');
   const fullDate = `${date}-${formattedDay}`;
   const URL = `${BACK_URL}/api/lectures/date?date=${fullDate}`;
-
   return wrapApiResponse(
     () => fetch(`${URL}`),
     (res) => res.json().then((dto: ResponseLectures) => dto),
@@ -42,74 +61,127 @@ const getLectures = async (date: string, day: number): Promise<ApiResponse<Respo
 
 // 컴포넌트
 const SectionList = ({ startDay, startDate }: Props) => {
-  const [lectureOption, setLectureOption] = useState<number>(startDay); // 강의 옵션 상태
-  const [lectures, setLectures] = useState<Lectures>([]);
+  const [option, setOption] = useState<CategoryWithAll>('ALL');
   const [loading, setLoading] = useState<boolean>(true);
   const [prevNum, setPrevNum] = useState<number>(5);
+  const limitRef = useRef<boolean>(false);
+  const prevOptionRef = useRef<CategoryWithAll | null>(null);
   const { wishlist } = useLectureStore();
 
-  // 캐싱
-  const cacheRef = useRef<Map<string, Lectures>>(new Map());
+  const initialCategoryState = (Object.keys(CategoryLabel) as CategoryWithAll[]).reduce(
+    (acc, key) => {
+      acc[key] = {
+        load: false,
+        lectures: [],
+        totalCount: 0,
+      };
+      return acc;
+    },
+    {} as CategoryState,
+  );
 
-  // 세션 데이터 가져오기
-  useEffect(() => {
-    const cacheKey = `${startDate}-${lectureOption}`;
-    (async () => {
-      setLoading(true);
+  const [lectures, setLectures] = useState<CategoryState>({ ...initialCategoryState });
 
-      // 데이터 캐싱 처리
-      if (cacheRef.current.has(cacheKey)) {
-        const cachedData = cacheRef.current.get(cacheKey)!;
-        setLectures(cachedData);
-        setPrevNum(cachedData.length);
-        return;
-      }
+  const getInitData = useCallback(async () => {
+    setLoading(true);
+    try {
+      const res = await getLectures(startDate, startDay);
 
-      try {
-        const res = await getLectures(startDate, lectureOption);
+      if (!res.data) throw new Error('데이터 없음');
 
-        if (!res.data) throw new Error('데이터 없음');
+      const data = res.data as ResponseLectures;
 
-        const data = res.data as ResponseLectures;
-        cacheRef.current.set(cacheKey, data.lectures);
-        setLectures(data.lectures);
+      setLectures((prev) => ({
+        ...prev,
+        ALL: {
+          ...data,
+          load: true,
+        },
+      }));
+
+      startTransition(() => {
         setPrevNum(data.totalCount);
-        setLoading(false);
-      } catch (error) {
-        console.error('강의 로드 실패: 테스트 데이터로 대체', error);
-      }
-    })();
-  }, [lectureOption, startDate]);
+      });
 
-  // 옵션 변경시 로딩 초기화
+      limitRef.current = true;
+      setLoading(false);
+    } catch (error) {
+      console.error('강의 로드 실패: 테스트 데이터로 대체', error);
+    }
+    return;
+  }, [setLoading, setPrevNum, setLectures, startDay, startDate]);
+
+  const getFilteringData = useCallback(() => {
+    setLoading(true);
+    const filteringDto = lectures.ALL.lectures.filter((lecture) => lecture.category === option);
+    setLectures((prev) => ({
+      ...prev,
+      [option]: {
+        lectures: filteringDto,
+        totalCount: filteringDto.length,
+        load: true,
+      },
+    }));
+
+    startTransition(() => {
+      setPrevNum(filteringDto.length);
+    });
+
+    setTimeout(() => {
+      setLoading(false);
+    }, 200);
+  }, [setLoading, setLectures, lectures, option]);
+
   useEffect(() => {
-    setLoading(false);
-  }, [lectureOption]);
+    const prevOption = prevOptionRef.current;
+    if (option === prevOption) return;
+    prevOptionRef.current = option;
+
+    // 초기값 받아옴
+    if (!limitRef.current) getInitData();
+
+    // 로드가 안됨... 리밋 초기화 (사용자가 옵션 변경시 불러옴)
+    if (!lectures.ALL.load) {
+      limitRef.current = false;
+      return;
+    }
+
+    // 캐싱된 값이 있다면 아이템 갯수 반영
+    if (lectures[option].load) {
+      setPrevNum((prev) => {
+        if (prev === lectures[option].totalCount) return prev;
+        return lectures[option].totalCount;
+      });
+      return;
+    }
+
+    getFilteringData();
+  }, [option, lectures, getFilteringData, getInitData]);
 
   return (
     <div>
       <OptionList
-        startDay={startDay}
-        option={lectureOption}
-        ChangeOption={(option: number) => setLectureOption(option)}
+        option={option}
+        ChangeOption={(option: CategoryWithAll) => {
+          setOption(option);
+        }}
       />
-      {/* 강의 카드 리스트 */}
-      <ScrollWrapper className="mt-3">
-        {loading && Array.from({ length: prevNum }).map((_, i) => <SkeletonCard key={i} />)}
-
-        {lectures.map((lecture, idx) => (
-          <Card
-            key={idx}
-            id={lecture.id}
-            image={lecture.thumbnailUri}
-            title={lecture.title}
-            time={`${formatTime(lecture.startTime)} ~ ${formatTime(lecture.endTime)}`}
-            category={lecture.category}
-            speakerName={lecture.speakerName}
-            isWished={wishlist.has(lecture.id)}
-            isProfile={false}
-          />
-        ))}
+      <ScrollWrapper className="mt-3 h-[405.19px]">
+        {loading
+          ? Array.from({ length: prevNum }).map((_, idx) => <SkeletonCard key={idx} />)
+          : lectures[option].lectures.map((lecture) => (
+              <Card
+                key={lecture.id}
+                id={lecture.id}
+                image={lecture.thumbnailUri}
+                title={lecture.title}
+                time={`${formatTime(lecture.startTime)} ~ ${formatTime(lecture.endTime)}`}
+                category={lecture.category}
+                speakerName={lecture.speakerName}
+                isWished={wishlist.has(lecture.id)}
+                isProfile={false}
+              />
+            ))}
       </ScrollWrapper>
     </div>
   );
