4cc3dec449991104aba01530f3c805876ec22eab
ki2183 <ki2183@naver.com>
2025-03-25 22:39:31 +0900

    feat(filesave-subscribe): 스크롤 기능 및 알림 감지 컴포넌트 추가
    - 새 메시지 감지 시 알림을 표시하는 컴포넌트 추가
    - 스크롤이 최하단이 아닐 경우, 좌측 하단에 하단 이동 버튼 표시
    - 버튼 클릭 시 알림 창의 최하단으로 스크롤 이동
    - 알림 창 열릴 때 자동으로 스크롤 최하단 이동
    
    Resolve: #107

--- src/app/_components/Header/Alarm/AlarmButton.tsx
+++ src/app/_components/Header/Alarm/AlarmButton.tsx
@@ -1,11 +1,13 @@
 import useHeaderStore from '@/_store/Header/useHeaderStore';
 import useAlarmStore from '@/_store/Header/useAlarmStore';
 import AlarmSvg from '@/assets/Header/alarm.svg';
+import ClickButton from '@/_components/ClickButton';
 
 // 알림 버튼 컴포넌트
 export const AlarmButton = ({ scale = 32 }: { scale?: number }) => {
   const setIsOpen = useAlarmStore((store) => store.isOpen.actions.setIsOpen);
   const setCloseMobile = useHeaderStore((store) => store.mobileNavOpen.actions.setClose);
+  const isNewMessage = useAlarmStore((store) => store.isNewMessage.state.isNewMessage);
 
   const openModal = () => {
     setCloseMobile();
@@ -13,9 +15,17 @@ export const AlarmButton = ({ scale = 32 }: { scale?: number }) => {
   };
 
   return (
-    <button className="leading-[140%]" aria-label="open-modal" onClick={openModal}>
-      <AlarmSvg alt="alarm-icon" width={scale} height={scale} />
-    </button>
+    <ClickButton
+      actionDesc="open-alarmWindow"
+      onClick={openModal}
+      aria-label="open-modal"
+      className="leading-[140%] cursor-pointer relative"
+    >
+      <AlarmSvg width={scale} height={scale} />
+      {isNewMessage && (
+        <div className="absolute lef w-[9px] h-[9px] bottom-[0px] right-[2px] bg-red-500 rounded-full" />
+      )}
+    </ClickButton>
   );
 };
 export default AlarmButton;
--- src/app/_components/Header/Alarm/AlarmList.tsx
+++ src/app/_components/Header/Alarm/AlarmList.tsx
@@ -1,5 +1,8 @@
 import useAlarmStore from '@/_store/Header/useAlarmStore';
 import { Alarm } from '@/_types/Header/Alarm.type';
+import { UIEvent, useCallback, useEffect, useRef, useState } from 'react';
+import DownSVG from '@/assets/Main/down.svg';
+import ClickButton from '@/_components/ClickButton';
 type ItemProps = Alarm;
 
 const SEC_MINUTE = 60;
@@ -21,8 +24,38 @@ export const getAlarmTime = (time: string) => {
 
 const AlarmList = () => {
   const alarms = useAlarmStore((store) => store.alarms.state.alarms);
+  const isOpen = useAlarmStore((store) => store.isOpen.state.isOpen);
+  const [isEnd, setIsEnd] = useState<boolean>(false);
+  const ulRef = useRef<HTMLUListElement>(null);
+  const SCROLL_THRESHOLD = 100;
+
+  // 스크롤이 끝인지 아닌지 판별
+  const scrollHandler = useCallback((e: UIEvent<HTMLUListElement>) => {
+    const { scrollTop, scrollHeight, clientHeight } = e.currentTarget;
+    const isBottom = scrollTop >= scrollHeight - clientHeight - SCROLL_THRESHOLD;
+    setIsEnd(isBottom);
+  }, []);
+
+  const scrollToBottom = useCallback(({ smooth = false }: { smooth?: boolean }) => {
+    const ul = ulRef.current;
+    if (!ul) return;
+    ul.scrollTo({
+      top: ul.scrollHeight,
+      behavior: smooth ? 'smooth' : 'auto',
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!isOpen) return;
+    scrollToBottom({ smooth: false });
+  }, [isOpen, scrollToBottom]);
+
   return (
-    <ul className="h-[calc(100vh-64px)] md:h-[calc(100vh-80px)] p-6 pt-6 overflow-y-scroll hide-scrollbar">
+    <ul
+      onScroll={scrollHandler}
+      ref={ulRef}
+      className="h-[calc(100vh-64px)] md:h-[calc(100vh-80px)] p-6 pt-6 overflow-y-scroll hide-scrollbar relative"
+    >
       {alarms.map(({ title, contents, createdAt }, idx) => (
         <AlarmItem
           key={`${title} + ${idx}`}
@@ -31,14 +64,18 @@ const AlarmList = () => {
           createdAt={createdAt}
         />
       ))}
-      {alarms.map(({ title, contents, createdAt }, idx) => (
-        <AlarmItem
-          key={`${title} + ${idx}`}
-          title={title}
-          contents={contents}
-          createdAt={createdAt}
-        />
-      ))}
+
+      {!isEnd && (
+        <ClickButton
+          actionDesc="scroll-end"
+          onClick={() => {
+            scrollToBottom({ smooth: true });
+          }}
+          className="fixed z-[10000] w-8 h-8 shadow-[0_0_0_1.5px_#6a728249] cursor-pointer bg-[#f8f8f8] text-gray-400 rounded-full overflow-hidden right-[4px] bottom-[20px] hover:brightness-90"
+        >
+          <DownSVG className="pt-[2.5px]" width={32} height={32} />
+        </ClickButton>
+      )}
     </ul>
   );
 };
@@ -48,11 +85,11 @@ const AlarmItem = ({ createdAt, title, contents }: ItemProps) => {
     <li className="w-full my-6 text-sm md:text-base">
       <div className="flex justify-between">
         <span className=" font-bold">{title}</span>
-        <span className="text-xs md:text-sm font-medium">{getAlarmTime(createdAt)}</span>
+        <span className="text-xs md:text-sm font-medium ">{getAlarmTime(createdAt)}</span>
       </div>
 
-      <div className="mt-2">
-        <span>{contents}</span>
+      <div className="mt-2 w-full">
+        <p className="break-words whitespace-normal w-full block">{contents}</p>
       </div>
     </li>
   );
