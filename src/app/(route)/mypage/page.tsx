0d0f1394b6dfe4314cb604053b02b6f398b0439b
ki2183 <ki2183@naver.com>
2025-03-25 15:10:23 +0900

    design(myPage): 모바일 최소 넓이 조정
    Resolves: #95

--- src/app/(route)/mypage/page.tsx
+++ src/app/(route)/mypage/page.tsx
@@ -3,8 +3,8 @@ import { LogoutButton, UserDeleteButton } from './component/handleButton';
 
 const Page = () => {
   return (
-    <div className="min-w-[343px] flex items-center justify-center md:h-[calc(100vh-80px)] w-screen max-w-[788px] mx-auto bg-white">
-      <div className=" py-16 px-[40px] h-auto w-full flex flex-col mt-16 text-sm md:text-base">
+    <div className="flex overflow-hidden items-center justify-center md:h-[calc(100vh-80px)] w-screen max-w-[788px] mx-auto bg-white">
+      <div className="min-w-[343px] py-16 px-[40px] h-auto w-full flex flex-col mt-16 text-sm md:text-base">
         {/* 정보 */}
         <div className="flex md:flex-row flex-col gap-6">
           <MyProileImage />
