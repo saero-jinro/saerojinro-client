82df4fa7bef128257e0385a7d4b69e1660a35298
hjoo830 <hjoo830@naver.com>
2025-03-26 20:26:55 +0900

    design: update lecture detail page UI

--- src/app/(route)/lecture/[id]/page.tsx
+++ src/app/(route)/lecture/[id]/page.tsx
@@ -6,9 +6,8 @@ import reviewResponse from '@/dummyData/lecture/getReview.json';
 import questionResponse from '@/dummyData/lecture/getQuestion.json';
 
 import TextViewer from '@/_components/TextViewer/TextViewer';
-import TestSVG from '@/assets/test/test.svg';
-import TestSVG2 from '@/assets/test/test2.svg';
-import UserSVG from '@/assets/test/user.svg';
+import DownloadSvg from '@/assets/Lecture/Download.svg';
+import UserSVG from '@/assets/Lecture/Regular.svg';
 import { TextareaHTMLAttributes, useEffect, useRef, useState } from 'react';
 import { contents } from '@/dummyData/lecture/getContents';
 import useHeaderStore from '@/_store/Header/useHeaderStore';
@@ -16,6 +15,7 @@ import LectureReserveButton from '@/_components/LectureReserveButton/LectureRese
 import useAuthStore from '@/_store/auth/useAuth';
 import useLoginModalStore from '@/_store/modal/useLoginModalStore';
 import { useTimetableStore } from '@/_store/timetable/useTimetableStore';
+import WishButton from '@/_components/Wish/WishButton';
 
 // 크기 자동 조절 textArea
 const Textarea = ({
@@ -108,42 +108,35 @@ const LectureDetailPage = () => {
       {reviews && questions && <div className="read-only hidden">{lectureId}</div>}
 
       {/* 헤더 */}
-      <header className="flex flex-col items-start gap-[40px] self-stretch">
-        <div className="flex flex-col gap-4 ">
-          {/* 시간 */}
+      <header className="flex flex-col items-start self-stretch border-b border-b-[#E2E8F0] pb-10">
+        <div className="flex flex-col gap-3">
           <time>
-            <span className="rounded-[4px] py-1 px-2 text-sm font-semibold text-[#1677ff] border border-solid border-[#91CAFF]">
-              Day1 09:00 ~ 10:00 Room A-1
+            <span className="rounded-sm py-1 px-2 text-sm font-medium text-[#2F78FF] bg-[#E6EFFF]">
+              Day1 | 09:00 ~ 10:00 | Room A-1
             </span>
           </time>
-          {/* 제목 */}
           <h1 className="font-bold text-[32px]">{lecture.title}</h1>
           <div className="flex items-center gap-[8px]">
-            <span className="px-4 py-2 font-semibold text-[#384559]">#{lecture.category}</span>
+            <span className="font-semibold text-base text-[#2F78FF]">#{lecture.category}</span>
           </div>
         </div>
       </header>
 
-      {/* 컨텐츠 */}
       <article>
         <TextViewer content={contents} />
       </article>
 
       {/* 강사 소개 */}
-      <article className="flex items-center gap-6 self-stretch">
+      <article className="flex gap-6 self-stretch border-t border-t-[#E2E8F0] pt-10">
         <div>
-          {/* 강사 이미지 */}
-          <div className="bg-gray-300 w-[78px] aspect-square" />
+          <div className="bg-gray-300 w-[78px] aspect-square rounded-sm" />
         </div>
         <div className="flex flex-col flex-[1_0_0] gap-3">
-          <div>
-            {/* name */}
-            <span className="font-bold mr-3">파크민수</span>
-            {/* email */}
-            <span>park@naver.com</span>
+          <div className="flex item-center">
+            <span className="font-bold text-base mr-3">파크민수</span>
+            <span className="font-normal text-base">park@naver.com</span>
           </div>
-          {/* 강사 소개 */}
-          <p className="whitespace-pre-line">
+          <p className="whitespace-pre-line font-normal text-base">
             {`김지훈 강사는 10년 이상의 개발 경험을 보유한 소프트웨어 엔지니어로, 현재 ABC 테크놀로지에서 개발팀장을 맡고 있습니다. 
               Vue.js와 Spring Boot를 활용한 풀스택 개발을 전문으로 하며, 실무에서의 경험을 바탕으로 쉽고 실용적인 개발 강의를 제공합니다. 
               다수의 컨퍼런스 발표 및 기술 블로그 운영을 통해 최신 개발 트렌드를 공유하고 있습니다.`}
@@ -151,10 +144,9 @@ const LectureDetailPage = () => {
         </div>
       </article>
 
-      {/* 다운로드 버튼 */}
       <div>
-        <button className="btn rounded-[8px] py-2 px-4 flex flex-row items-center justify-center gap-2 font-semibold     ">
-          <TestSVG width={16} height={16} />
+        <button className="btn rounded-xs py-[6px] px-4 flex flex-row items-center justify-center gap-2 font-semibold text-base bg-white text-[#015AFF] border border-[#015AFF]    ">
+          <DownloadSvg />
           download
         </button>
       </div>
@@ -163,8 +155,7 @@ const LectureDetailPage = () => {
       <div className="flex flex-col gap-6">
         <h2 className="text-2xl font-bold self-stretch">사전 질문</h2>
         <div className="flex gap-6">
-          {/* 질문 입력 */}
-          <div className="flex items-center justify-centerresize-none border w-full border-[#1677FF] py-3 rounded-lg shadow-[0px_0px_0px_2px_rgba(5,145,255,0.10)]">
+          <div className="flex items-center justify-center resize-none w-full bg-[#F1F5F9] text-[#BDBDBD] px-4 rounded-xs">
             <Textarea
               placeholder="강연자에게 질문하고 싶은 것을 입력해주세요. 등록된 사전질문은 작성자와 강연자에게만 보입니다."
               className="w-full border-none min-h-6 overflow-hidden resize-none border px-3 rounded-md focus:outline-none"
@@ -173,34 +164,27 @@ const LectureDetailPage = () => {
               rows={1}
             />
           </div>
-          {/* 등록 버튼 */}
-          <button className="btn font-semibold rounded-[8px] p-4 w-[78px] h-[53px] overflow-hidden">
+          <button className="btn font-semibold text-base rounded-xs px-4 py-1 w-[62px] h-12 overflow-hidden">
             등록
           </button>
         </div>
 
-        {/* 리?뷰 사전질문 */}
         <div className="flex items-start gap-6 self-stretch relative">
           <div className="absolute flex h-[46px] w-[78px] items-center gap-1">
-            {/* 이미지 */}
-            <div className="w-8 h-8 bg-gray-300 rounded-[6px] gap-1">
+            <div className="w-8 h-8 gap-1">
               <UserSVG />
             </div>
-            {/* 닉네임 */}
             <div>
-              <span className="">닉네임</span>
+              <span className="font-semibold text-base">닉네임</span>
             </div>
           </div>
-          {/* 내용 */}
           <div className="flex-1 pt-8 mt-2 md:pt-0 md:mx-[calc(72px+24px+24px)]">
-            {/* 그냥 댓글 */}
             <p>
               도배 도배 도배 도배 도배 도배 도배 도배 도배 도배 도배 도배 도배 도배 도배 도배 도배
               도배 도배 도배 도배 도배 도배 도배 도배 도배 도배 도배 도배 도배 도배 도배 도배 도배
               도배 도배 도배 도배 도배 도배 도배 도배 도배 도배 도배 도배 도배 도배 도배 도배 도배
             </p>
           </div>
-          {/* 수정/삭제 */}
           <div className="absolute h-[46px] right-0 flex gap-6 items-center ">
             <button>수정</button>
             <button>삭제</button>
@@ -210,18 +194,14 @@ const LectureDetailPage = () => {
         {/* 수정 가능 댓글 */}
         <div className="flex items-start gap-6 self-stretch relative">
           <div className="flex w-[78px] h-[46px] items-center gap-1 absolute ">
-            {/* 이미지 */}
-            <div className="w-8 h-8 bg-gray-300 rounded-[6px] gap-1">
+            <div className="w-8 h-8 gap-1">
               <UserSVG />
             </div>
-            {/* 닉네임 */}
             <div>
-              <span className="">닉네임</span>
+              <span className="font-semibold text-base">닉네임</span>
             </div>
           </div>
-          {/* 내용 */}
           <div className="flex-1 pt-8 mt-2 md:pt-0 md:mx-[calc(72px+24px+24px)]">
-            {/* 수정 가능 댓글 */}
             <div className="flex items-center justify-center w-full">
               <Textarea
                 value={review}
@@ -231,21 +211,25 @@ const LectureDetailPage = () => {
               />
             </div>
           </div>
-          {/* 수정/삭제 */}
           <div className="absolute h-[46px] right-0 flex gap-6 items-center">
             <button>수정</button>
             <button>삭제</button>
           </div>
         </div>
 
-        {/* 버튼 */}
-        <div className="flex flex-row gap-2 items-center">
-          <button className="bg-[#EFF6FF] p-2 rounded-[4px] h-10">
-            <TestSVG2 />
-          </button>
+        <div className="flex flex-row gap-2 items-center mt-3">
+          <div className="flex flex-col justify-center items-center bg-[#F1F5F9] w-12 h-12">
+            <WishButton
+              isWished={true}
+              itemId={1}
+              className="pt-1"
+              iconClassName="w-6 h-6 text-[#015AFF]"
+            />
+            <span className="text-[#015AFF] font-bold text-sm">999</span>
+          </div>
           <LectureReserveButton
             isReserved={isReserved}
-            className="w-full bg-[#155DFC] py-[23px] mt-3"
+            className="w-full bg-[#155DFC] h-12"
             onClick={async (e) => {
               e.stopPropagation();
 
