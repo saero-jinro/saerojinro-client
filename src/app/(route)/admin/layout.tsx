14fc18413fc99b2621d4d76a88b7296650d4e2b8
junaanjuna <yellowbyme0503@gmail.com>
2025-03-26 20:15:32 +0900

    style: 어드민 대시보드 UI 수정

--- src/app/(route)/admin/layout.tsx
+++ src/app/(route)/admin/layout.tsx
@@ -1,6 +1,6 @@
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex h-screen">
+    <div className="flex h-screen bg-[#E2E8F0]">
       {/* 콘텐츠 영역 - 헤더와의 간격 64px 설정 */}
       <main className="flex-1 p-6 pt-16">{children}</main>
     </div>
