6950d89d438d3514d88223cbcfe903c3d4d2d16d
ki2183 <ki2183@naver.com>
2025-03-31 11:37:48 +0900

    feat(etc): 헤더 다크모드 아이콘 삭제
    - 헤더 다크모드 제어 버튼 삭제
    - 마이페이지 다크모드 제어 컴포넌트 추가
    
    Resolve: #158

--- src/app/(route)/mypage/page.tsx
+++ src/app/(route)/mypage/page.tsx
@@ -1,10 +1,13 @@
 import { MyProfileEmail, MyProfileName, MyProileImage } from './component/dataComponents';
 import { LogoutButton, UserDeleteButton } from './component/handleButton';
+import getTheme from '@/_utils/Header/getTheme.server';
+import DarkMode from './component/DarkMode.server';
 
-const Page = () => {
+const Page = async () => {
+  const theme = await getTheme();
   return (
     <div className="flex overflow-hidden py-16 px-[40px] bg-white dark:bg-[#070A12] min-h-screen items-center justify-center md:h-auto w-screen max-w-[1280px] mx-auto ">
-      <div className="min-w-[343px] h-full w-full md:max-w-[788px] flex flex-col mt-16 text-sm md:text-base">
+      <div className="min-w-[343px] h-full w-full md:max-w-[788px] flex flex-col gap-10 mt-16 text-sm md:text-base">
         {/* 정보 */}
         <div className="flex md:flex-row flex-col gap-6">
           <MyProileImage />
@@ -14,6 +17,9 @@ const Page = () => {
             <MyProfileEmail />
           </div>
         </div>
+
+        {/* 다크모드 */}
+        <DarkMode theme={theme} />
         {/* 로그아웃 */}
         <LogoutButton />
         {/* 회원탈퇴 */}
