b103f029208e7bd12b9bf5dfe89583ccda9f3b3e
ki2183 <ki2183@naver.com>
2025-03-31 18:08:48 +0900

    feat(timetable/querystring): 강의 리스트 페이지 훅 적용 기존 코드 삭제
    Resolve: #167

--- src/app/(route)/lecture-list/page.tsx
+++ src/app/(route)/lecture-list/page.tsx
@@ -25,25 +25,19 @@ const Page = async ({
   const { day, category } = await searchParams;
 
   // 날짜 유효성 체크
-  if (!day) {
-    redirect(`/lecture-list/?day=Day1&category=ALL`);
-  }
+  if (!day) redirect(`/lecture-list/?day=Day1&category=ALL`);
 
   const isValidDay = /^Day\d+$/.test(day);
   const dayNum = Number(day.replace('Day', ''));
 
   // 날짜가 해당하는 날짜인지 확인
-  if (!isValidDay || dayNum < 1 || dayNum > LIMITDAY) {
-    redirect('/');
-  }
+  if (!isValidDay || dayNum < 1 || dayNum > LIMITDAY) redirect('/');
 
   const categories = category?.split(',') ?? ['ALL'];
   const isValidCategories = categories.every((c) => allowedCategories.includes(c));
 
   // 카테고리 유효성 체크
-  if (!isValidCategories) {
-    redirect(`/lecture-list/?day=${day}&category=ALL`);
-  }
+  if (!isValidCategories) redirect(`/lecture-list/?day=${day}&category=ALL`);
 
   return <LectureList initDay={day} initCategories={categories} />;
 };
