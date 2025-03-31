25321f1ac045d45cd94fcf10ad31242b2960edf2
hjoo830 <hjoo830@naver.com>
2025-03-31 15:07:25 +0900

    fix: allow kakao cdn for profile image

--- next.config.ts
+++ next.config.ts
@@ -24,6 +24,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'img1.kakaocdn.net',
       },
+      {
+        protocol: 'https',
+        hostname: 'k.kakaocdn.net',
+      },
     ],
   },
 };
