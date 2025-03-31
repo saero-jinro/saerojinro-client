ce36199f9805784e87f9ef822fb9790f7a328106
ki2183 <ki2183@naver.com>
2025-03-31 11:04:29 +0900

    design(fix): 텍스트 사이즈 수정
    Resolve: #158

--- src/app/_components/Card/SkeletonCard.tsx
+++ src/app/_components/Card/SkeletonCard.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 const SkeletonCard = () => {
   return (
-    <div className=" transition-opacity duration-200 overflow-hidden flex justify-center items-center w-[282px] h-[401.19px] bg-white dark:bg-[#070A12] border border-[#DEE2E6] dark:border-[#161F2E] rounded-xl cursor-pointer">
+    <div className=" transition-opacity duration-200 overflow-hidden flex justify-center items-center w-[282px] h-[410px] max-md:w-[165px] max-md:h-[295px] bg-white dark:bg-[#070A12] border border-[#DEE2E6] dark:border-[#161F2E] rounded-xl cursor-pointer">
       <Image
         src="/main/loading.gif"
         alt={'loading'}
